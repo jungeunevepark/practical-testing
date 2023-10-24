e2bd0a1d817838f4b155604896dd47969097e718
jungeunevepark <evecomcom98@naver.com>
2023-10-24 17:01:40 +0900

    feat: 판매 가능 타입인 상품들을 반환하는 JPA 함수 선언

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -1,7 +1,15 @@
 package sample.cafekiosk.spring.domain.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	
+
+	/*
+	 * select *
+	 * from product
+	 * where selling_type in ('SELLING', 'HOLD');
+	 * */
+	List<Product> findAllBySellingTypeIn(List<ProductSellingType> sellingTypes);
 }
