cae903d1106c41d92bf71e2f8cc88633462edf1d
jungeunevepark <evecomcom98@naver.com>
2023-10-24 17:02:38 +0900

    feat: 판매 가능한 상품들을 반환하는 서비스 함수 구현

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,9 +1,15 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingType;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +17,11 @@ public class ProductService {
 
 	private final ProductRepository productRepository;
 
-	public void getSellingProducts() {
+	public List<ProductResponse> getSellingProducts() {
+		List<Product> products = productRepository.findAllBySellingTypeIn(ProductSellingType.forDisplay());
 
+		return products.stream()
+			.map(ProductResponse::of)
+			.collect(Collectors.toList());
 	}
 }
