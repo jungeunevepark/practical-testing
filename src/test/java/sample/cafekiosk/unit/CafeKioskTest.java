333abcd4fd676d57f0a94253e23e47cb13a56835
jungeunevepark <evecomcom98@naver.com>
2023-10-24 12:46:05 +0900

    test: 여러 잔을 주문에 대한 테스트 코드 작성

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -9,26 +9,35 @@ import sample.cafekiosk.unit.beverage.Latte;
 
 public class CafeKioskTest {
 	@Test
-	void add_manual_test(){
+	void add_manual_test() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		cafeKiosk.add(new Americano());
 
-		System.out.println(">>>> 담긴 음료 수 : "+cafeKiosk.getBeverages().size());
-		System.out.println(">>>> 담긴 음료 : "+cafeKiosk.getBeverages().get(0).getName());
+		System.out.println(">>>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+		System.out.println(">>>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
 	}
 
 	@Test
-	void add(){
+	void add() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		cafeKiosk.add(new Americano());
 
-		// assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
 		assertThat(cafeKiosk.getBeverages()).hasSize(1);
 		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
 	}
 
 	@Test
-	void remove(){
+	void addSeveralBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+		cafeKiosk.add(americano, 2);
+
+		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+		assertThat(cafeKiosk.getBeverages().get(1).getName()).isEqualTo("아메리카노");
+	}
+
+	@Test
+	void remove() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		Americano americano = new Americano();
 
@@ -40,7 +49,7 @@ public class CafeKioskTest {
 	}
 
 	@Test
-	void clear(){
+	void clear() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		Americano americano = new Americano();
 		Latte latte = new Latte();
