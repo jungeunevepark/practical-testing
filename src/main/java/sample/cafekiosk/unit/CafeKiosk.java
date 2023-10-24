ef532ae924906c03edc1cfe76e66a204633428cd
jungeunevepark <evecomcom98@naver.com>
2023-10-24 12:45:46 +0900

    feat: 여러 잔을 한번에 주문할 수 있는 함수 구현

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -12,25 +12,37 @@ import sample.cafekiosk.unit.order.Order;
 public class CafeKiosk {
 
 	private final List<Beverage> beverages = new ArrayList<>();
+
 	public void add(Beverage beverage) {
 		beverages.add(beverage);
 	}
 
-	public void remove(Beverage beverage){
+	public void add(Beverage beverage, int count) {
+		if (count <= 0) {
+			throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+		}
+		for (int i = 0; i < count; i++) {
+			beverages.add(beverage);
+		}
+	}
+
+	public void remove(Beverage beverage) {
 		beverages.remove(beverage);
 	}
-	public void clear(){
+
+	public void clear() {
 		beverages.clear();
 	}
+
 	public int calculateTotalPrice() {
 		int totalPrice = 0;
-		for(Beverage beverage: beverages){
+		for (Beverage beverage : beverages) {
 			totalPrice += beverage.getPrice();
 		}
 		return totalPrice;
 	}
 
-	public Order createOrder(){
+	public Order createOrder() {
 		return new Order(LocalDateTime.now(), beverages);
 	}
 }
