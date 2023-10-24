9f392f556852d45f4ea6db8177e703d9890c90fa
jungeunevepark <evecomcom98@naver.com>
2023-10-24 13:25:49 +0900

    fix: 주문 시간을 파라미터로 받도록 개선

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -46,8 +46,7 @@ public class CafeKiosk {
 		return totalPrice;
 	}
 
-	public Order createOrder() {
-		LocalDateTime currentDateTime = LocalDateTime.now();
+	public Order createOrder(LocalDateTime currentDateTime) {
 		LocalTime currentTime = currentDateTime.toLocalTime();
 
 		if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
