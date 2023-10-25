3eef48e9158c86a1c5036038bebd7d047e922ea3
jungeunevepark <evecomcom98@naver.com>
2023-10-25 13:34:54 +0900

    test: 함수가 추가됨에 따라 JPA 함수 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -58,4 +58,44 @@ class ProductRepositoryTest {
 				tuple("002", "카페라떼", ProductSellingType.HOLD)
 			);
 	}
+
+	@DisplayName("상품번호 리스트로 상품들을 조회한다.")
+	@Test
+	void findAllByProductNumberIn() {
+		Product product1 = Product.builder()
+			.productNumber("001")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.SELLING)
+			.name("아메리카노")
+			.price(4000)
+			.build();
+		Product product2 = Product.builder()
+			.productNumber("002")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.HOLD)
+			.name("카페라떼")
+			.price(4500)
+			.build();
+		Product product3 = Product.builder()
+			.productNumber("003")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.SOLDOUT)
+			.name("팥빙수")
+			.price(7000)
+			.build();
+
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		// when
+		List<Product> products = productRepository.findAllByProductNumberIn(
+			List.of("001", "002"));
+
+		// then
+		assertThat(products).hasSize(2)
+			.extracting("productNumber", "name", "sellingType")
+			.containsExactlyInAnyOrder(
+				tuple("001", "아메리카노", ProductSellingType.SELLING),
+				tuple("002", "카페라떼", ProductSellingType.HOLD)
+			);
+	}
 }
