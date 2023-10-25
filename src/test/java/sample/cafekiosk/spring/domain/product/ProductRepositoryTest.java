962a7bef1df62745c1dc0d8e7f6f90f5504536d2
jungeunevepark <evecomcom98@naver.com>
2023-10-25 10:12:44 +0900

    test: repository 단계에서의 테스트 코드 생성

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,61 @@
+package sample.cafekiosk.spring.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+// @SpringBootTest
+@ActiveProfiles("test")
+@DataJpaTest
+class ProductRepositoryTest {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
+	@Test
+	void findAllBySellingStatusIn() {
+		// given
+		Product product1 = Product.builder()
+			.productNumber("001")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.SELLING)
+			.name("아메리카노")
+			.price(4000)
+			.build();
+		Product product2 = Product.builder()
+			.productNumber("002")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.HOLD)
+			.name("카페라떼")
+			.price(4500)
+			.build();
+		Product product3 = Product.builder()
+			.productNumber("003")
+			.type(ProductType.HANDMADE)
+			.sellingType(ProductSellingType.SOLDOUT)
+			.name("팥빙수")
+			.price(7000)
+			.build();
+
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		// when
+		List<Product> products = productRepository.findAllBySellingTypeIn(
+			List.of(ProductSellingType.SELLING, ProductSellingType.HOLD));
+
+		// then
+		assertThat(products).hasSize(2)
+			.extracting("productNumber", "name", "sellingType")
+			.containsExactlyInAnyOrder(
+				tuple("001", "아메리카노", ProductSellingType.SELLING),
+				tuple("002", "카페라떼", ProductSellingType.HOLD)
+			);
+	}
+}
