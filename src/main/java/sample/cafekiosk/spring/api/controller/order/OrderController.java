9571173e5b02627de0f4b77764e23a09e248430f
jungeunevepark <evecomcom98@naver.com>
2023-10-25 13:49:00 +0900

    rename: 서비스 함수가 파라미터로 주문시간을 받음에 따라 파라미터 추가

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -1,5 +1,7 @@
 package sample.cafekiosk.spring.api.controller.order;
 
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ public class OrderController {
 
 	@PostMapping("/api/v1/orders/new")
 	public void createOrder(@RequestBody OrderCreateRequest request) {
-		orderService.createOrder(request);
+		orderService.createOrder(request, LocalDateTime.now());
 
 	}
 }
