02f152618ebc7913c255a68014ceaf2d2112c41e
jungeunevepark <evecomcom98@naver.com>
2023-10-26 10:23:26 +0900

    test: 재고 확인 함수에 대한 테스트 코드 작성

---
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -0,0 +1,23 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+
+	@DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다.")
+	@Test
+	void isQuantityLessThan() {
+		// given
+		Stock stock = Stock.create("001", 1);
+		int quantity = 2;
+
+		// when
+		boolean result = stock.isQuantityLessThan(quantity);
+
+		// then
+		assertThat(result).isTrue();
+	}
+}
