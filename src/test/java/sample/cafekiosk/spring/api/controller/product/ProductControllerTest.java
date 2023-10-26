2a66bc783a9fbf49fce45ffe80a812ef237837c6
jungeunevepark <evecomcom98@naver.com>
2023-10-26 15:19:11 +0900

    test: 로그를 볼 수 있도록 설정

--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -43,6 +44,7 @@ class ProductControllerTest {
 				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
 			)
+			.andDo(MockMvcResultHandlers.print())
 			.andExpect(MockMvcResultMatchers.status().isOk());
 
 	}
