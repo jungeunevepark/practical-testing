6c6aebf61ecc707d1ca7f72cc4412ec841f1ea5f
jungeunevepark <evecomcom98@naver.com>
2023-10-26 15:55:49 +0900

    fix: 규격화된 응답을 반환하도록 리턴값 수정

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -2,12 +2,16 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
@@ -18,9 +22,8 @@ public class ProductController {
 	private final ProductService productService;
 
 	@PostMapping("/api/v1/product/new")
-	public void createProduct(@RequestBody ProductCreateRequest request) {
-		productService.createProduct(request);
-
+	public ApiResponse<ProductResponse> createProduct(@RequestBody @Valid ProductCreateRequest request) {
+		return ApiResponse.ok(HttpStatus.OK, productService.createProduct(request));
 	}
 
 	@GetMapping("/api/v1/product/selling")
