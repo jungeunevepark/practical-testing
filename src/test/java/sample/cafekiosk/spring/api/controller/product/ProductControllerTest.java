f1e8aeff82ef96fe96d02be3e649bcfc4f0924ed
jungeunevepark <evecomcom98@naver.com>
2023-10-27 13:23:21 +0900

    test: GET 요청에 대해서도 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -1,7 +1,11 @@
 package sample.cafekiosk.spring.api.controller.product;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,12 +14,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.ProductSellingType;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
@@ -41,7 +45,7 @@ class ProductControllerTest {
 			.price(4000)
 			.build();
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product/new")
+		mockMvc.perform(post("/api/v1/product/new")
 				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
 			)
@@ -59,7 +63,7 @@ class ProductControllerTest {
 			.price(4000)
 			.build();
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product/new")
+		mockMvc.perform(post("/api/v1/product/new")
 				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
 			)
@@ -81,7 +85,7 @@ class ProductControllerTest {
 			.price(4000)
 			.build();
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product/new")
+		mockMvc.perform(post("/api/v1/product/new")
 				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
 			)
@@ -103,7 +107,7 @@ class ProductControllerTest {
 			.price(4000)
 			.build();
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product/new")
+		mockMvc.perform(post("/api/v1/product/new")
 				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
 			)
@@ -126,9 +130,10 @@ class ProductControllerTest {
 			.price(0)
 			.build();
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product/new")
-				.content(objectMapper.writeValueAsString(request))
-				.contentType(MediaType.APPLICATION_JSON)
+		mockMvc.perform(
+				post("/api/v1/product/new")
+					.content(objectMapper.writeValueAsString(request))
+					.contentType(MediaType.APPLICATION_JSON)
 			)
 			.andDo(print())
 			.andExpect(status().isBadRequest())
@@ -136,6 +141,25 @@ class ProductControllerTest {
 			.andExpect(jsonPath("$.status").value("BAD_REQUEST"))
 			.andExpect(jsonPath("$.message").value("상품 가격은 양수이어야 합니다."))
 			.andExpect(jsonPath("$.data").isEmpty());
+
+	}
+
+	@DisplayName("판매 상품을 조회한다.")
+	@Test
+	void getSellingProducts() throws Exception {
+
+		List<ProductResponse> result = List.of();
+		when(productService.getSellingProducts()).thenReturn(result);
+
+		mockMvc.perform(
+				get("/api/v1/product/selling")
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.code").value("200"))
+			.andExpect(jsonPath("$.status").value("OK"))
+			.andExpect(jsonPath("$.message").value("OK"))
+			.andExpect(jsonPath("$.data").isArray());
 
 	}
 }
