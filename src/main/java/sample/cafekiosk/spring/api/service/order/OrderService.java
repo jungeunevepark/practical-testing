1bf73902e4ab5a1149ccec57256c67fe8b5266c2
jungeunevepark <evecomcom98@naver.com>
2023-10-27 13:46:34 +0900

    fix: 서비스에서는 서비스 dto를 사용하도록 파라미터 변경

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.request.OrderServiceRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
@@ -33,7 +33,7 @@ public class OrderService {
 	 * 재고 감소 -> 동시성 고민 필요
 	 * optimistic lock / pessimistic lock / ...
 	 * */
-	public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredTime) {
+	public OrderResponse createOrder(OrderServiceRequest request, LocalDateTime registeredTime) {
 		List<String> productNumbers = request.getProductNumbers();
 		List<Product> products = findByProductsBy(productNumbers);
 
