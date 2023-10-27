a23983316633a694eaacd005041417b4fbe8b009
jungeunevepark <evecomcom98@naver.com>
2023-10-27 13:39:42 +0900

    feat: 생성 시, Request Dto의 상품 번호가 누락되지 않도록 @NotEmpty사용

--- src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.controller.order.request;
 
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class OrderCreateRequest {
+	@NotEmpty(message = "상품 번호 리스트는 필수입니다.")
 	private List<String> productNumbers;
 
 	@Builder
